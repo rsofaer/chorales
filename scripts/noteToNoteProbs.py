4baf3cbcbf082c006cb1d6c98544d6f8ec78b109
aflock <andrewbflockhart@gmail.com>
2011-12-23 15:49:18 -0500

    starting on this shit

---
+++ scripts/noteToNoteProbs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+'''
+File: noteToNoteProbs.py
+Author: AFlock
+Description: Given a JSON version of the chorales dataset,
+	output a dictionary of probability distributions for the next note.
+'''
+
+def probabalize(data):
+	"""Do the damn work"""
+
+
+
