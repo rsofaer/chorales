9a5ac7767164ea416997d592066f7b3e854a1aa5
aflock <andrewbflockhart@gmail.com>
2011-12-23 16:14:36 -0500

    first draft

--- scripts/noteToNoteProbs.py
+++ scripts/noteToNoteProbs.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+import pickle
+import json
+
 '''
 File: noteToNoteProbs.py
 Author: AFlock
@@ -8,7 +11,42 @@ Description: Given a JSON version of the chorales dataset,
 '''
 
 def probabalize(data):
-	"""Do the damn work"""
+	"""
+	 {"st": 8  ,  "pitch": 67,  "dur": 4 ,  "keysig": -1,  "timesig": 12,  "fermata": 0},
+	 {"st": 12  ,  "pitch": 69,  "dur": 8 ,  "keysig": -1,  "timesig": 12,  "fermata": 0},
+	"""
+	counts = {}
+	"""
+	counts = {69: {65: 1, 64: 2, ... }}
+	"""
+	#first count all the occurences in our data set
+	# we are tracking
+	for chorNum, notes in data.iteritems():
+		for i in range(len(notes)):
+			if notes[i+1]:
+				note = notes[i]
+				nextNote = notes[i+1]
+				counts[note['pitch']][nextNote['pitch']] += 1
+
+	#then normalize to get a 1-integral distribution
+	pitchprob = {}
+	for opitch, numbers in counts.iteritems():
+		sum = 0
+		for p, num in numbers.iteritems():
+			sum += num
+		for p, num in numbers.iteritems():
+			pitchprob[opitch][p] = num/sum
 
 
+	return pitchprobs
+
+if __name__ == '__main__':
+	#get Data
+	#normalize?
+	#run that shit
+	#pickle the result
+
+	probs = probabalize(data)
+
+	pickle.dump(probs, open("noteProbs.p", "wb"))
 
