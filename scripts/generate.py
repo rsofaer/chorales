9bed65ed33e150d6224ff35a1f237528169be5b0
Raphael Sofaer <raphael@joindiaspora.com>
2011-12-27 21:28:42 -0800

    I'm pretty sure this doesn't change any logic

--- scripts/generate.py
+++ scripts/generate.py
@@ -60,52 +60,11 @@ def distribution(pr):
         else:
             p[i] = 0
     def func(x):
-        #total = 0
-        #for x in range(0,20):
-        #    total += p.get(x)
-        #    if x < total:
-        #        return x
-        #return false
-        if x < p.get(0):
-            return 0
-        if x < p.get(0) + p.get(1):
-            return 1
-        if x < p.get(0) + p.get(1) + p.get(2):
-            return 2
-        if x < p.get(0) + p.get(1) + p.get(2) + p.get(3):
-            return 3
-        if x < p.get(0) + p.get(1) + p.get(2) + p.get(3) + p.get(4):
-            return 4
-        if x < p.get(0) + p.get(1) + p.get(2) + p.get(3) + p.get(4) + p.get(5):
-            return 5
-        if x < p.get(0) + p.get(1) + p.get(2) + p.get(3) + p.get(4) + p.get(5) + p.get(6):
-            return 6
-        if x < p.get(0) + p.get(1) + p.get(2) + p.get(3) + p.get(4) + p.get(5) + p.get(6) + p.get(7):
-            return 7
-        if x < p.get(0) + p.get(1) + p.get(2) + p.get(3) + p.get(4) + p.get(5) + p.get(6) + p.get(7) + p.get(8):
-            return 8
-        if x < p.get(0) + p.get(1) + p.get(2) + p.get(3) + p.get(4) + p.get(5) + p.get(6) + p.get(7) + p.get(8) + p.get(9):
-            return 9
-        if x < p.get(0) + p.get(1) + p.get(2) + p.get(3) + p.get(4) + p.get(5) + p.get(6) + p.get(7) + p.get(8) + p.get(9) + p.get(10):
-            return 10
-        if x < p.get(0) + p.get(1) + p.get(2) + p.get(3) + p.get(4) + p.get(5) + p.get(6) + p.get(7) + p.get(8) + p.get(9) + p.get(10) + p.get(11):
-            return 11
-        if x < p.get(0) + p.get(1) + p.get(2) + p.get(3) + p.get(4) + p.get(5) + p.get(6) + p.get(7) + p.get(8) + p.get(9) + p.get(10) + p.get(11) + p.get(12):
-            return 12
-        if x < p.get(0) + p.get(1) + p.get(2) + p.get(3) + p.get(4) + p.get(5) + p.get(6) + p.get(7) + p.get(8) + p.get(9) + p.get(10) + p.get(11) + p.get(12) + p.get(13):
-            return 13
-        if x < p.get(0) + p.get(1) + p.get(2) + p.get(3) + p.get(4) + p.get(5) + p.get(6) + p.get(7) + p.get(8) + p.get(9) + p.get(10) + p.get(11) + p.get(12) + p.get(13) + p.get(14):
-            return 14
-        if x < p.get(0) + p.get(1) + p.get(2) + p.get(3) + p.get(4) + p.get(5) + p.get(6) + p.get(7) + p.get(8) + p.get(9) + p.get(10) + p.get(11) + p.get(12) + p.get(13) + p.get(14) + p.get(15):
-            return 15
-        if x < p.get(0) + p.get(1) + p.get(2) + p.get(3) + p.get(4) + p.get(5) + p.get(6) + p.get(7) + p.get(8) + p.get(9) + p.get(10) + p.get(11) + p.get(12) + p.get(13) + p.get(14) + p.get(15)+ p.get(16):
-            return 16
-        if x < p.get(0) + p.get(1) + p.get(2) + p.get(3) + p.get(4) + p.get(5) + p.get(6) + p.get(7) + p.get(8) + p.get(9) + p.get(10) + p.get(11) + p.get(12) + p.get(13) + p.get(14) + p.get(15)+ p.get(16)+ p.get(17):
-            return 17
-        if x < p.get(0) + p.get(1) + p.get(2) + p.get(3) + p.get(4) + p.get(5) + p.get(6) + p.get(7) + p.get(8) + p.get(9) + p.get(10) + p.get(11) + p.get(12) + p.get(13) + p.get(14) + p.get(15)+ p.get(16)+ p.get(17)+ p.get(18):
-            return 18
-        if x < p.get(0) + p.get(1) + p.get(2) + p.get(3) + p.get(4) + p.get(5) + p.get(6) + p.get(7) + p.get(8) + p.get(9) + p.get(10) + p.get(11) + p.get(12) + p.get(13) + p.get(14) + p.get(15)+ p.get(16)+ p.get(17)+ p.get(18)+ p.get(19):
-            return 19
+        total = 0
+        for i in range(0,20):
+            total += p.get(i)
+            if x < total:
+                return i
         return false
     return func
 #}}}
