b2de1c2e29589ae75e7f536732e5f35e4dfccacc
aflock <andrewbflockhart@gmail.com>
2011-12-29 19:54:35 -0500

    one giant json son

--- scripts/midiTojson.py
+++ scripts/midiTojson.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 import json
 import mingus
 import mingus.midi.MidiFileIn as mi
@@ -8,43 +8,47 @@ File: midiTojson.py
 Author: AFlock
 Description: turns a midi to a json like we're used to. Requires mingus
 '''
-def main(file):
-    try:
-        composition = mi.MIDI_to_Composition(file)
-    except Error as e:
-        print e
+def main():
+    datadir = "../dataset/fourPartChorales/"
+    filter = "b_.mid"
+    allChorales = {}
+    for filename in os.listdir(datadir):
+        if filter in filename:
+            file = datadir+filename
+            try:
+                composition = mi.MIDI_to_Composition(file)
+            except Error as e:
+                print e
 
-    #print composition
-    tempo = composition[1]
+            #print composition
+            tempo = composition[1]
 
-    print tempo
-    compositionList = []
-    for voice in composition[0]:
-        noteSequence = []
-        st = 0
-        for bar in voice:
-            for note in bar:
-                dur = 16/note[1]
-                pitch = note[2]
-                if len(pitch) < 1:
-                    continue
-                pitch = pitch[0]
-                pitchNum = int(pitch)
-                noteDict = {"st":st, "pitch" : pitchNum, "dur" : dur, "keysig": None, "timesig" : None, "fermata": 0}
-                noteSequence.append(noteDict)
-                st += dur
-        compositionList.append(noteSequence)
+            #print tempo
+            compositionList = []
+            for voice in composition[0]:
+                noteSequence = []
+                st = 0
+                for bar in voice:
+                    for note in bar:
+                        dur = 16/note[1]
+                        pitch = note[2]
+                        if len(pitch) < 1:
+                            continue
+                        pitch = pitch[0]
+                        pitchNum = int(pitch)
+                        noteDict = {"st":st, "pitch" : pitchNum, "dur" : dur, "keysig": None, "timesig" : None, "fermata": 0}
+                        noteSequence.append(noteDict)
+                        st += dur
+                compositionList.append(noteSequence)
+            allChorales[filename] = compositionList
 
 
-    fname = "../dataset/fourPartJSON/" + file.split("/")[-1] + ".json"
+    fname = "../dataset/fourPartJSON.json"
     print "writing to ", fname
-    json.dump(compositionList, open(fname, "wb"))
+    json.dump(allChorales, open(fname, "wb"))
 
 
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print "midiTojson.py <filename>"
-        exit()
-    main(sys.argv[1])
+    main()
