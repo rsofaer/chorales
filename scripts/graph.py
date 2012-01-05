7b2ba47b2ecafb75c41d645d0ea20b8a3b2bafb6
aflock <andrewbflockhart@gmail.com>
2012-01-05 01:10:30 -0500

    graph expanded

--- scripts/graph.py
+++ scripts/graph.py
@@ -10,8 +10,10 @@ import json
 
 from noteToNoteProbs import probabalize
 
-#Global of all chords
-chord_list = json.loads(open("../dataset/chord_list", "rb"))
+#Global cleandata
+cleandata = json.load(open("../dataset/cleandata.json", "rb"))
+#chord_list = json.loads(open("../dataset/chord_list", "rb"))
+
 
 
 """
@@ -20,7 +22,7 @@ Note: chords here are usually tuples with ([list,of,notes], #energy)
 """
 class chordNode():
     """Has a chord, a number of incoming energies and a node energy """
-    def __init__(chord):
+    def __init__(self, chord):
         self.chord = chord[0]
         self.energy = chord[1]
         #outbound is a list of all following chords and the energies to go from one to the next
@@ -31,24 +33,20 @@ class chordNode():
 
 class layer():
     """Holds all possible chords along with weights for each"""
-    def __init__():
+    def __init__(self):
         self.chords = []
         for chord in chord_list:
             self.chords.append(chordNode(chord))
 
 
-class graph():
+class Graph():
     """ holds layers"""
-    def __init__(duration= 40)
-    data = json.load(open("../dataset/cleandata.json", "rb"))
-    self.probs = probabalize(data)
-    self.layers = []
-    for i in range(duration):
-        self.layers.append(layer())
-
-def genGraph():
-    """creates a directed graph of possible chord progression with energies a"""
-
+    def __init__(self, duration= 40):
+        self.chord_e = ChordEnergy(cleandata)
+        self.probs = probabalize(data)
+        self.layers = []
+        for i in range(duration):
+            self.layers.append(layer())
 
 
 def cross_energy(origin, outbound):
@@ -63,7 +61,10 @@ def cross_energy(origin, outbound):
 
 
 
-
+if __name__ == '__main__':
+    g = Graph(10)
+    print g.generateChorale()
+    #print g.whatever_else
 
 
 
